c49846b7477eeb60f194f88144c3bd8b8e90f3d8
skaigroove <skaigroove@gmail.com>
2024-02-12 00:16:55 +0900

    AppConfig를 통해 인스턴스 생성하여, App,Test 코드에 DIP를 만족해줄 수 있도록 코드 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,5 @@
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.member.MemberRepository;
+package hello.core;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,10 +9,11 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig { // 관리자 설정 // cmd + e : 과거 히스토리 파일이 다 나온다 recent file
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); // MemoryMemberRepository가 생성되고,
+        return new MemberServiceImpl(new MemoryMemberRepository()); // MemoryMemberRepository가 생성
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()) ;
+        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy()) ;
     }
 }
+
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,22 +3,23 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.order.Order;
 import hello.core.order.OrderService;
 
-
-public class MemberApp {
+public class OrderApp {
 
     public static void main(String[] args) {
 
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
-//      MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L,"memberA", Grade.VIP);
+        Long memberId = 1L;
+        Member member = new Member(memberId,"memberA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("find Member = " + findMember.getName());
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+
+        System.out.println("order = " + order);
     }
 }
