7487482c166c9e98e29b706629d3d326922b9a98
skaigroove <skaigroove@gmail.com>
2024-02-22 17:03:04 +0900

    example of singletonContainer + SingletonService.java , + SingletonTest.java

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -9,7 +9,8 @@ public class BeanDefinitionTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
     //    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
     @Test
-    @DisplayName("빈 설정 메타정보 확인") void findApplicationBean() {
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
